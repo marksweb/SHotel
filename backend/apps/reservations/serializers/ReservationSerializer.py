dec396c5d85be1a44f36f3cb8ad23366f471713a
Seweryn Piórkowski <50875213+SewerynPiorkowski@users.noreply.github.com>
2020-07-17 18:17:19 +0200

    set model validation for reservation serializer
    set model validation for reservation serializer
    
    update

--- backend/apps/reservations/serializers/ReservationSerializer.py
+++ backend/apps/reservations/serializers/ReservationSerializer.py
@@ -3,7 +3,12 @@ from rest_framework import serializers
 from ..models import Reservation
 
 
-class ReservationSerializer(serializers.HyperlinkedModelSerializer):
+class ReservationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Reservation
         fields = '__all__'
+
+    def validate(self, validated_data):
+        instance = Reservation(**validated_data)
+        instance.clean()
+        return validated_data
