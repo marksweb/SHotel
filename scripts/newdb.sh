284870d4fa34632b8135a3cf4feaaf1c532fa0c3
Seweryn Piórkowski <50875213+SewerynPiorkowski@users.noreply.github.com>
2020-07-20 10:17:18 +0200

    improve scripts

--- scripts/newdb.sh
+++ scripts/newdb.sh
@@ -1,5 +1,3 @@
-cd ..
-cd backend
 docker exec -it shotel_backend pipenv run python manage.py makemigrations
 docker exec -it shotel_backend pipenv run python manage.py migrate
 docker exec -it shotel_backend pipenv run python manage.py runscript new_db
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,2 +1,1 @@
-cd ..
 docker-compose down
